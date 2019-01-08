edcfcce90803bc3bb6072554053c5c597c3468f2
DScode51 <fordavidsmithcode@gmail.com>
2019-01-08 17:52:31 +0200

    notification page

---
+++ notification.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+	<head>
+	<!-- Required meta tags -->
+	<?php include 'pills/cssLink.php' ?>
+
+	<title>A propos</title>
+	</head>
+<body>
+	
+
+	<div class="col-10 offset-1 col-md-8 offset-md-2" style="margin-top: 100px;">
+		<h5 class="text-center">Nous vous avons envoyé un mail contenant un lien pour réinitialiser votre mot de passe. Veuillez consulter votre boîte mail.</h5>
+	</div>
+
+	
+</body>
+</html>
